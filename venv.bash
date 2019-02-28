120917b4fd5f2b7e816a1a515ffa089e9e027810
Brian McCallister <brianm@skife.org>
2019-02-28 20:52:52 +0000

    Better `venv use` with subshell

--- venv.bash
+++ venv.bash
@@ -3,16 +3,15 @@
 __venv_prompt() {
     case $TERM in
         "xterm-color")
-            echo "(\[\033[${VENV_PROMPT_COLOR}m\]$(basename "$VIRTUAL_ENV")\[\033[00m\])";;
+            echo "\[\033[${VENV_PROMPT_COLOR}m\]$(basename "$VIRTUAL_ENV")\[\033[00m\]";;
         "xterm-256color")
-            echo "(\[\033[${VENV_PROMPT_COLOR}m\]$(basename "$VIRTUAL_ENV")\[\033[00m\])";;
+            echo "\[\033[${VENV_PROMPT_COLOR}m\]$(basename "$VIRTUAL_ENV")\[\033[00m\]";;
         *)
             echo "($(basename "$VIRTUAL_ENV"))";;
     esac
 }
 
 venv() {
-
     if [ ! -n "${VENV_DIR}" ] 
     then
         VENV_DIR="${HOME}/.venv"
@@ -24,11 +23,11 @@ venv() {
     
     case "$1" in
         "create")
-            /usr/local/share/python/virtualenv --prompt='$(__venv_prompt)'  "$VENV_DIR/$2";;
+            python -m venv --prompt='$(__venv_prompt)'  "$VENV_DIR/$2";;
         "destroy")
             rm -rf "$VENV_DIR/$2";;
         "use")
-            source "$VENV_DIR/$2/bin/activate";;
+            $SHELL -i <<< "source .venv/$2/bin/activate; exec </dev/tty"
         "ls")
             ls "$VENV_DIR";;
         *)
@@ -59,6 +58,15 @@ __venv_completion() {
 
 
 complete -F __venv_completion venv
+
+for script in $1/*; do
+    
+    # skip non-executable snippets
+    [ -x "$script" ] || continue
+    
+    # execute $script in the context of the current shell
+    . $script
+done
 
 # use venv based virtual env by default if it exists
 if [ -d ~/.venv/default ] ; then
